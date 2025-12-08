d5149fd365e21d13c250ce0e7beb62d38895af4f
Stack Quantum <stackquantum1@gmail.com>
2025-12-08 14:16:51 -0800

    Updating User Interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}", // adjust if you have other folders/files
+    "./*.html",
+    "./src/**/*.{html,js}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
